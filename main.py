726e7332662dc1fade09117aeba3867c3b2334aa
Christina <chirochristina@gmail.com>
2025-01-25 08:41:16 -0800

    add hosting abuse info

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import whois
 import socket
 
-# phase 1: rough testing
-# res = whois.whois("inconcert.cl")
-# res2 = whois.whois("https://tabulation.co/")
-#
-# print(res2.registrar_url)
-# print(res.registrar_url)
-# print(res2)
-#
-# value = socket.gethostbyname("inconcert.cl")
-# print(value)
-#
+#using whois for now, will want to move to using rdap in the future
 
-#moved into a very basic function
+###TBD - Get DNS Records
+
+
 def get_info(domain_name):
-    domain_registar = whois.whois(domain_name)
+    #whois lookup
+    domain_registrar = whois.whois(domain_name)
+    #need to parse results for emails in the case no abuse email is listed
+
+    # abuse_emails = []
+    # emails = domain_registrar.get("emails", [])
+    # if isinstance(emails, str):  # Handle single email case
+    #     emails = [emails]
+    # abuse_emails = [email for email in emails if "abuse" in email]
+    #
+    # #hosting provider finder
     hosting_provider_ip = socket.gethostbyname(domain_name)
-    # print (domain_registar, hosting_provider)
-    results = [domain_registar, hosting_provider]
+###TBD - ipinfo lookup
+    # Generate IPinfo link
+    ipinfo_url = f"https://ipinfo.io/{hosting_provider_ip}"
+    results = [domain_registrar, hosting_provider_ip, ipinfo_url]
     return results
 
-
 results = get_info("www.lukeandlouise.com")
-results = get_info("yieldscredit.com")
-results = get_info("tabulation.co")
-print(results[0])
+# results = get_info("yieldscredit.com")
+# results = get_info("tabulation.co")
+print(results[2])
