055fabbf08bfa0b6dda9983a968a60e0f4f4eb25
Christina <chirochristina@gmail.com>
2025-01-25 11:44:31 -0800

    added list emails feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import whois
 import socket
 
-#using whois for now, will want to move to using rdap in the future
+
+#using whois for now, will want to move to using rdap in the future,
+#with a whois fallback when tld does not support rdap
 
 ###TBD - Get DNS Records
 
@@ -17,15 +19,29 @@ def generate_ipinfo_link(ip_address):
     return "no info"
 
 def get_domain_registrar_info(domain_name):
-    # whois lookup
+    # whois lookup, want abuse contact
     domain_registrar = whois.whois(domain_name)
-    # need to parse results for emails in the case no abuse email is listed
-    # abuse_emails = []
-    # emails = domain_registrar.get("emails", [])
-    # if isinstance(emails, str):  # Handle single email case
-    #     emails = [emails]
-    # abuse_emails = [email for email in emails if "abuse" in email]
-    return domain_registrar
+
+    #registrar urls
+    registrar_url = domain_registrar.get("registrar_url")
+    registrar_name = domain_registrar.get("registrar")
+
+    #sort and list emails
+    emails = domain_registrar.get('emails', [])
+    if isinstance(emails, str):  # If 'emails' is a single string
+        emails = [emails]  # Convert it to a list
+
+    abuse_emails = [email for email in emails if "abuse" in email.lower()]
+    other_emails = [email for email in emails if email not in abuse_emails]
+    return {
+        "registrar_info": {
+            "name": registrar_name or "Not Available",
+            "url": registrar_url or "Not Available",
+        },
+        "abuse_emails": abuse_emails,
+        "other_emails": other_emails
+    }
+
 
 def get_info(domain_name):
     domain_registrar = get_domain_registrar_info(domain_name)
@@ -40,7 +56,19 @@ def get_info(domain_name):
     # }
     return results
 
+# get_domain_registrar_info('www.holidaypartyevents.com')
+
 # results = get_info("www.lukeandlouise.com")
 # results = get_info("yieldscredit.com")
-results = get_info("tabulation.co")
-print(results[1])
+# results = get_info("tabulation.co")
+# print(results[1])
+
+# domain_lookup = whois.whois('www.holidaypartyevents.com')
+# name = domain_lookup.get("registrar", [])
+# registrar_url = domain_lookup.get("registrar_url", [] )
+# print(name, registrar_url)
+
+
+# NOTES on abuse contact formats:
+#emails": "abuse@dnspod.com
+#sometimes no emails, fall back to registar url
