1015298da2dea3adf00bedc9c55445e7a5ece42e
Christina <chirochristina@gmail.com>
2025-01-26 13:44:25 -0800

    routing and jinja2 syntax

--- app.py
+++ app.py
@@ -1,29 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, render_template, request
 from main import get_info
-#tell flask where application is located
+
 app = Flask(__name__)
 
-
-# @app.route('/info', methods=['GET'])
-
-# def info_endpoint():
-#     # Get the domain from the query string
-#     domain = request.args.get('domain')
-#     if not domain:
-#         return jsonify({"error": "Domain parameter is required"}), 400
-#
-#     # Call get_info to handle everything
-#     result = get_info(domain)
-#
-#     # Return the result as JSON
-#     return jsonify(result)
-
-
-# basic for stlying sake
 @app.route('/')
 def home():
-    # Render the basic index.html page
     return render_template('index.html')
+
+@app.route('/info', methods=['GET'])
+def info():
+    # Get the domain from the query parameter
+    domain = request.args.get('domain')
+
+    # Call your get_info function to fetch data
+    data = get_info(domain)
+
+    # Render the result page with the data
+    return render_template('index.html', data=data)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
